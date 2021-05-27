f3c882faaf8bfbc69d67d37250ce2b8527c23dba
Jia-Jingnan <875480307@qq.com>
2021-05-28 02:48:01 +0800

    0528-定义一个常量类，所有的常量会放在该类中，通过该类调用

---
+++ src/main/java/com/lilith/galamall/common/Const.java
@@ -0,0 +1,11 @@
+package com.lilith.galamall.common;
+
+/**
+ * @Author:JiaJingnan
+ * @Date: 上午2:45 2021/5/28
+ * 常量类
+ */
+public class Const {
+
+    public static final String CURRENT_USER = "currentUser";
+}
