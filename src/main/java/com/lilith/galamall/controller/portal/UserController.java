f92904e715548e9017a62ee678f9c517e25df4e2
Jia-Jingnan <875480307@qq.com>
2021-05-28 11:02:06 +0800

    0528-添加Swagger注解，并完成登出方法

--- src/main/java/com/lilith/galamall/controller/portal/UserController.java
+++ src/main/java/com/lilith/galamall/controller/portal/UserController.java
@@ -4,12 +4,16 @@ import com.lilith.galamall.common.Const;
 import com.lilith.galamall.common.GalaRes;
 import com.lilith.galamall.entity.User;
 import com.lilith.galamall.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpSession;
 
 /**
@@ -18,11 +22,14 @@ import javax.servlet.http.HttpSession;
  */
 @RestController
 @RequestMapping("user")
+@Api(tags = "用户模块")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
+
+    @ApiOperation(value = "用户名密码登陆", httpMethod = "POST")
     @PostMapping("/login")
     public GalaRes<User> login(String username, String password, HttpSession session){
 
@@ -31,6 +38,14 @@ public class UserController {
             session.setAttribute(Const.CURRENT_USER, res.getData());
         }
         return res;
+    }
+
+
+    @ApiOperation(value = "登出", httpMethod = "GET")
+    @GetMapping("/logout")
+    public GalaRes<String> logout(HttpSession session){
+        session.removeAttribute(Const.CURRENT_USER);
+        return GalaRes.createBySuccess();
     }
 
 
