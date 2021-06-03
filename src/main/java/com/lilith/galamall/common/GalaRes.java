f2e62712c7301f855f5e81e5a4d811a73d606b4f
Jia-Jingnan <875480307@qq.com>
2021-06-04 01:42:41 +0800

    0604-maven依赖及包更新

--- src/main/java/com/lilith/galamall/common/GalaRes.java
+++ src/main/java/com/lilith/galamall/common/GalaRes.java
@@ -1,8 +1,10 @@
 package com.lilith.galamall.common;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
 import lombok.Getter;
+import org.codehaus.jackson.annotate.JsonIgnore;
+import org.codehaus.jackson.map.annotate.JsonSerialize;
+
 
 import java.io.Serializable;
 
