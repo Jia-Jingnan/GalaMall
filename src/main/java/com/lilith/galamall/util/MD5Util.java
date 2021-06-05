a3061cbaa1cc6dc69727861ec7d83e22a97bdc48
Jia-Jingnan <875480307@qq.com>
2021-06-05 11:16:49 +0800

    0605-本次提交可以忽略

--- src/main/java/com/lilith/galamall/util/MD5Util.java
+++ src/main/java/com/lilith/galamall/util/MD5Util.java
@@ -54,4 +54,9 @@ public class MD5Util {
     private static final String hexDigits[] = {"0", "1", "2", "3", "4", "5",
             "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"};
 
+    public static void main(String[] args) {
+        String adminmd5 = MD5EncodeUtf8("admin111");
+        System.out.println(adminmd5);
+    }
+
 }
