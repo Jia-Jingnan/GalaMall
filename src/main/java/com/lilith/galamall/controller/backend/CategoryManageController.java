374348b7e28131c3222791d8b1ba7e33db3b6b64
Jia-Jingnan <875480307@qq.com>
2021-06-05 10:29:38 +0800

    0605-增加CategoryManageController类，新增addCategory方法

---
+++ src/main/java/com/lilith/galamall/controller/backend/CategoryManageController.java
@@ -0,0 +1,53 @@
+package com.lilith.galamall.controller.backend;
+
+import com.lilith.galamall.common.Const;
+import com.lilith.galamall.common.GalaRes;
+import com.lilith.galamall.common.ResponseCode;
+import com.lilith.galamall.entity.User;
+import com.lilith.galamall.service.CategoryService;
+import com.lilith.galamall.service.UserService;
+import javafx.scene.Parent;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
+
+/**
+ * @Author:JiaJingnan
+ * @Date: 上午2:05 2021/5/28
+ */
+@Controller
+@RequestMapping("/manage/category")
+public class CategoryManageController {
+
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(value = "/add_category.do", method = RequestMethod.POST)
+    public GalaRes addCategory(HttpSession session, String categoryName,
+                               @RequestParam(value = "parentId", defaultValue = "0") int paraentId){
+        // 校验是否登陆
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
+        if (user == null){
+            return GalaRes.createByErrorCodeMessage(ResponseCode.NEEG_LOGIN.getCode(),"用户未登陆，请登陆");
+        }
+        // 校验是否未管理员
+        if (userService.checkAdmin(user).isSuccess()){
+
+            // 增加分类操作
+            return categoryService.addCategory(categoryName, paraentId);
+        } else {
+            return GalaRes.createByErrorMessage("无权限操作，需要管理员登陆");
+        }
+
+    }
+
+}
